7c329afc55803036068491a3e202e2e9243d3e7a
Anushreebasics <anushreebondia@gmail.com>
2024-08-27 15:04:21 +0530

    created vercel.json

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -2,11 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB9bDsX7w-KQWvFPL9SmfIzBVoO8ClcfM8",
   authDomain: "ai-trip-planner-2117f.firebaseapp.com",
